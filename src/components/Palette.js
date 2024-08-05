41440de0ed0633cc31562c3e59896ca7f7c513fa
slavakazak <slavakazak3@gmail.com>
2024-08-05 14:41:34 +0300

    change profile

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -10,21 +10,22 @@ export default function Palette({ player }) {
 
   return (
     <div className="palette">
-      <div className={"profile" + (active ? ' active' : '')} onClick={clickHandler}>{active ? player.name : player.name.substring(0, 2)}</div>
-      {player.loss ?
-        <div className="text">Пас</div>
-        :
-        <>
-          <div className="cards">
-            {player.palette.map((card, i) => <Card key={i} {...card} />)}
-          </div>
-          <div className={'score' + (player.win ? ' win' : '')}>
-            <span>{player.numOfWinCards}</span>
-            {player.maxWinCard ? <span style={{ color: player.maxWinCard.color.hue }}>{player.maxWinCard.value}</span> : null}
-          </div>
-        </>
+      <div className="profile" onClick={clickHandler}>{player.name.substring(0, 2)}</div>
+      {active ?
+        <div className="profile-card">{player.name.substring(0, 20)}</div>
+        : player.loss ?
+          <div className="text">Пас</div>
+          :
+          <>
+            <div className="cards">
+              {player.palette.map((card, i) => <Card key={i} {...card} />)}
+            </div>
+            <div className={'score' + (player.win ? ' win' : '')}>
+              <span>{player.numOfWinCards}</span>
+              {player.maxWinCard ? <span style={{ color: player.maxWinCard.color.hue }}>{player.maxWinCard.value}</span> : null}
+            </div>
+          </>
       }
-
     </div>
   )
 }
